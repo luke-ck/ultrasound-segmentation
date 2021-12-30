1710c8c6480cec8a4005f3cc01e5437d41698f05
Lucas Cosier <9642742+luke-ck@users.noreply.github.com>
2021-12-30 17:25:40 +0100

    new generator

--- data_generator.py
+++ data_generator.py
@@ -9,20 +9,29 @@ class CustomDataGenerator(Iterator):
         self.preprocessing_fn = preprocessing_fn
         super().__init__(X.shape[0], batch_size, shuffle, seed)
         self.generator = ImageDataGenerator(**kwargs)
-        
+
     def _get_batches_of_transformed_samples(self, index_array):
         """Gets a batch of transformed samples from array of indices"""
 
         # Get a batch of image data
-        
+
         copy_x = self.X[index_array].copy()
         copy_y = self.Y[index_array].copy()
         batch_x = np.zeros((len(index_array), *self.target_size, 1))
         batch_y = np.zeros((len(index_array), *self.target_size, 1))
+        y_has_mask = np.zeros(len(index_array))
         # Transform the inputs and correct the outputs accordingly
         for i, (x, y) in enumerate(zip(copy_x, copy_y)):
             transform_params = self.generator.get_random_transform(x.shape)
             t_x = self.generator.apply_transform(x, transform_params)
             t_y = self.generator.apply_transform(y, transform_params)
             batch_x[i], batch_y[i] = self.preprocessing_fn(t_x, denoise=True), self.preprocessing_fn(t_y)
-        return batch_x, batch_y
+            if np.count_nonzero(t_y) > 0:
+                y_has_mask[i] = 1
+
+        outputs = {
+            'aux_output': y_has_mask,
+            'main_output': batch_y
+        }
+
+        return batch_x, outputs
